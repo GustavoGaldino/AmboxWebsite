923aa8d95257a963890ebcd43b94a93f490a9bad
galdinocgm <gcgm@cin.ufpe.br>
2020-08-14 23:41:57 -0300

    Added about-us section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,7 +5,40 @@ import './styles.css'
 function AboutUs(){
     return(
         <div className = "about-us-div" id="about-us">
+
+            <div className="firstPurpleLine" />
+            <h1 className="ambox-title">
+                O QUE É A AMBOX?
+            </h1>
+
+            <div className="coffee"></div>
+            <div className="energy-drink"></div>
+            <div className="smoothie"></div>
+            <div className="pepsi"></div>
             
+            <div className="right-side-container">
+
+                <center>
+                    <div className="secondPurpleLine"></div>
+                </center>
+
+                <div className="Sentence">
+                    <h2>Somos um clube de assinatura com o objetivo de levar novas experiências para você.</h2>
+                </div>
+                
+                <div className="Sentence">
+                    <h2>A cada mês, uma surpresa diferente, uma sensação única.</h2>
+                </div>
+
+                <div className="Sentence">
+                    <h2>Saia do óbvio, experimente!</h2>
+                </div>
+
+                <center>
+                    <div className="thirdPurpleLine"></div>
+                </center>
+            </div>
+
         </div>
     );
 }
--- src/components/NavbarLinks/index.jsx
+++ src/components/NavbarLinks/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import CustomLink from '../CustomLink'
+import CustomLink from './CustomLink'
 
 import './styles.css'
 

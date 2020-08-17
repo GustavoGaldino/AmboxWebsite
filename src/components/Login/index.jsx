12610754e92ddbe1d27eddeb7b14474dd37e2b02
galdinocgm <gcgm@cin.ufpe.br>
2020-08-16 22:45:20 -0300

    Added login section

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
+import './styles.css'
+
 export default function Login(){
     return(
-        <div>
-            <h2>Oi</h2>
+        <div className="login-div">
+            <div className="login-form">
+                <div className="welcome">
+                    <h1>Bem-vindo</h1>
+                </div>
+                <div className="horizontal-line" />
+
+                <div class="input">
+                    <input type="text" placeholder="Email" class="input-field"/>
+                </div>
+
+                <div class="input second-input">
+                    <input type="text" placeholder="Senha" class="input-field"/>
+                </div>
+
+                <div className="user-login-btn-div">
+                    <button>
+                        <a href="">
+                            Entrar
+                        </a>
+                    </button>
+                </div>
+
+                <div className="register-div">
+                    Ainda não é um membro?
+                    <a href="" className="register">Cadastre-se</a>
+                </div>
+            </div>
         </div>
     );
 }

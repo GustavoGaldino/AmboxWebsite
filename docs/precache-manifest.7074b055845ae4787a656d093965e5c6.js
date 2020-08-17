cd016b77179095d0b36defff2df9344dd0505d6f
galdinocgm <gcgm@cin.ufpe.br>
2020-08-17 16:01:20 -0300

    Add docs folder for github pages

---
+++ docs/precache-manifest.7074b055845ae4787a656d093965e5c6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276d5dc05f8661011e03dc9de0f9df6a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2291aafb911d961db14e",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e20e00f82b743ba95ec2",
+    "url": "./static/css/main.c668f650.chunk.css"
+  },
+  {
+    "revision": "2291aafb911d961db14e",
+    "url": "./static/js/2.3576085d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.3576085d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20e00f82b743ba95ec2",
+    "url": "./static/js/main.7d00ee37.chunk.js"
+  },
+  {
+    "revision": "3c2b0a599046ced3b4ec",
+    "url": "./static/js/runtime-main.0f61816e.js"
+  },
+  {
+    "revision": "271073ac2ea9e0a550b7608c2f3741e3",
+    "url": "./static/media/coffee.271073ac.png"
+  },
+  {
+    "revision": "d88213945025edf1e4b1b3b208da3140",
+    "url": "./static/media/cup.d8821394.png"
+  },
+  {
+    "revision": "b426d58b21d2e23781b804ad0c6a8f4b",
+    "url": "./static/media/energy-drink.b426d58b.png"
+  },
+  {
+    "revision": "cd4fa6ead46e6bf43863af152cbab221",
+    "url": "./static/media/pepsi.cd4fa6ea.png"
+  },
+  {
+    "revision": "8e4708537a41eac62d3976fa047c53c6",
+    "url": "./static/media/smoothie.8e470853.png"
+  },
+  {
+    "revision": "3c218b4f071b5331866418a03c5454f4",
+    "url": "./static/media/star.3c218b4f.png"
+  },
+  {
+    "revision": "9087da9a55d0ac8f6f5a106e1494ccde",
+    "url": "./static/media/unboxing.9087da9a.png"
+  }
+]);
